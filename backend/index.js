2a9478fae559b59aa42c96ba9ff5783e474e9426
chinmayakrishnab <74190291+chinmayakrishnab@users.noreply.github.com>
2021-05-24 16:03:59 +0530

    footer,notification

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 const DBName = `ondemand`;
 // mongoose.connect(process.env.MONGODB_TOKEN, {
-    mongoose.connect(`mongodb+srv://user:1234@cluster0.tr3cr.mongodb.net/ondemand?retryWrites=true&w=majority`, {
+    mongoose.connect(`mongodb+srv:, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
