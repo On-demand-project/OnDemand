7479c7cb5607803a4d57fd2a0f9d7ddc17472922
Sujanram <sujanram0@gmail.com>
2021-06-26 21:14:08 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const logger = require('./middleware/logger');
 const service = require('./routes/service');
- 
+const path =require('path')
 
 const app = express();
 const PORT = 8000;
@@ -52,6 +52,17 @@ io.on('connection', socket => {
       io.to(users[from]).emit('message', { to,from, message })
     })
   })
+
+
+  if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,'../frontend/build')));
+  
+    app.get('/*',(req,res)=>{
+      res.sendFile(path.join(__dirname,'../frontend/build/index.html'));
+    })
+  
+  }
+
 
 
 http.listen(process.env.PORT || PORT, () =>
